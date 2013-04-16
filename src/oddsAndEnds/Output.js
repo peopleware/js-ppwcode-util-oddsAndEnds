1a45d32502190f56402aa7955aab872ffdd29cbf
Jan Dockx <Jan_Dockx@peopleware.be>
2013-04-16 14:04:35 +0000

    optional missing string
    git-svn-id: https://ppwcode.googlecode.com/svn/javascript/util/oddsAndEnds/trunk@6755 463e38ab-4f4e-0410-907a-65d2b3888964

--- src/oddsAndEnds/Output.js
+++ src/oddsAndEnds/Output.js
@@ -9,6 +9,11 @@ define(["dojo/_base/declare",
       //   is that the shown text is xml-escaped before it is put
       //   in the HTML.
 
+      // missing: string
+      //   This string is used when there is no value to show.
+      missing: "?value?",
+      // TODO re-render when missing changes
+
       postCreate: function() {
         this._output();
       },
@@ -20,7 +25,7 @@ define(["dojo/_base/declare",
         //		protected
         var outputNode = this.srcNodeRef || this.domNode;
         var output = this.templateString ? this._exprRepl(this.templateString) : this.value;
-        outputNode.innerHTML = xml.escape(output);
+        outputNode.innerHTML = xml.escape(output, false, this.missing);
       }
 
     });
--- src/oddsAndEnds/xml.js
+++ src/oddsAndEnds/xml.js
@@ -5,20 +5,25 @@ define([],
       // summary:
       //   Methods to aid with XML.
 
-      escape: function(/*String*/ str, /*Boolean?*/ noSingleQuotes){
+      escape: function(/*String*/ str, /*Boolean?*/ doNotEscapeSingleQuotes, /*String?*/ missing){
         // summary:
         //	 Adds escape sequences for special characters in XML: `&<>"'`.
         //	 Optionally skips escapes for single quotes.
+        // doNotEscapeSingleQuotes: Boolean?
+        //   Do not escape single quotes when this is true.
+        // missing: String?
+        //   When str is undefined, or null, we return missing. The default is the empty string.
         // description:
         //   Nicked from dijit/_editor/html.
         //   When str is undefined or null, return "".
         //   Actual non-string values are coerced to a string.
+
         if (!str && str != 0) { // 0 must pass
-          return "";
+          return missing || "";
         }
         str = "" + str; // coerce to string
         str = str.replace(/&/gm, "&amp;").replace(/</gm, "&lt;").replace(/>/gm, "&gt;").replace(/"/gm, "&quot;");
-        if(!noSingleQuotes){
+        if(!doNotEscapeSingleQuotes) {
           str = str.replace(/'/gm, "&#39;");
         }
         return str; // string
