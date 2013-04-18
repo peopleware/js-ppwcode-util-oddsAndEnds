54cf007f9afafe963e7b1b05f92bf6b1cea90d23
Jan Dockx <Jan_Dockx@peopleware.be>
2013-04-18 10:53:11 +0000

    dangling work of yesterday (untested): general lang support in js typeOf now deprecated
    git-svn-id: https://ppwcode.googlecode.com/svn/javascript/util/oddsAndEnds/trunk@6758 463e38ab-4f4e-0410-907a-65d2b3888964

---
+++ src/oddsAndEnds/js.js
@@ -0,0 +1,71 @@
+define([],
+  function() {
+
+    function typeOf(obj) {
+      // summary:
+      //   A better type then Object.toString() or typeof.
+      // description:
+      //      toType(undefined); //"undefined"
+      //      toType(new); //"null"
+      //      toType({a: 4}); //"object"
+      //      toType([1, 2, 3]); //"array"
+      //      (function() {console.log(toType(arguments))})(); //arguments
+      //      toType(new ReferenceError); //"error"
+      //      toType(new Date); //"date"
+      //      toType(/a-z/); //"regexp"
+      //      toType(Math); //"math"
+      //      toType(JSON); //"json"
+      //      toType(new Number(4)); //"number"
+      //      toType(new String("abc")); //"string"
+      //      toType(new Boolean(true)); //"boolean"
+
+      /* based on
+       http://javascriptweblog.wordpress.com/2011/08/08/fixing-the-javascript-typeof-operator/
+       */
+
+      var result = Object.prototype.toString.call(obj).match(/\s([a-zA-Z]+)/)[1].toLowerCase();
+      // on some browsers, the main window returns as "global" (WebKit) or "window" (FF), but this is an object too
+      if (result === "global" || result == "window") {
+        result = "object";
+      }
+      return result; // return String
+    }
+
+    function getPrototypeChain(/*Object*/ obj) {
+      // summary:
+      //   Returns an array where result[i] is the prototype
+      //   of result[i-1], with obj === result[0]. result[length]
+      //   is thus Object.prototype.
+
+      function recursive(acc, o) {
+        return (o === Object.prototype) ? acc.push(o) :  recursive(acc.push(o), Object.getPrototypeOf(obj));
+      }
+
+      return recursive([], obj);
+    }
+
+    function getAllKeys(/*Object*/ obj) {
+      // summary:
+      //   Returns an array containing the names of all given objects enumerable properties.
+      //   Like keys, but for the entire prototype chain. The array starts with the properties
+      //   of Object.prototype, and works down the chain.
+
+      return reduceRight(
+        function(acc, proto) {
+          return acc.concat(Object.keys(proto));
+        },
+        []
+      );
+    }
+
+    var js = {
+      // summary:
+      //   Methods to aid with the JavaScript language.
+      typeOf: typeOf,
+      getPrototypeChain: getPrototypeChain,
+      getAllKeys: getAllKeys
+    };
+
+    return js;
+  }
+);
--- src/oddsAndEnds/typeOf.js
+++ src/oddsAndEnds/typeOf.js
@@ -1,36 +1,8 @@
-define([],
-  function() {
+define(["./js.js"],
+  function(js) {
+    // summary:
+    //   Deprecated. Use js.typeOf instead.
 
-    function toType(obj) {
-      // summary:
-      //   A better type then Object.toString() or typeof.
-      // description:
-      //      toType(undefined); //"undefined"
-      //      toType(new); //"null"
-      //      toType({a: 4}); //"object"
-      //      toType([1, 2, 3]); //"array"
-      //      (function() {console.log(toType(arguments))})(); //arguments
-      //      toType(new ReferenceError); //"error"
-      //      toType(new Date); //"date"
-      //      toType(/a-z/); //"regexp"
-      //      toType(Math); //"math"
-      //      toType(JSON); //"json"
-      //      toType(new Number(4)); //"number"
-      //      toType(new String("abc")); //"string"
-      //      toType(new Boolean(true)); //"boolean"
-
-      /* based on
-       http://javascriptweblog.wordpress.com/2011/08/08/fixing-the-javascript-typeof-operator/
-       */
-
-      var result = Object.prototype.toString.call(obj).match(/\s([a-zA-Z]+)/)[1].toLowerCase();
-      // on some browsers, the main window returns as "global" (WebKit) or "window" (FF), but this is an object too
-      if (result === "global" || result == "window") {
-        result = "object";
-      }
-      return result; // return String
-    }
-
-    return toType;
+    return js.typeOf;
   }
 );
