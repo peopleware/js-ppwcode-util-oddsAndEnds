4c3b8d05a9859ffca8c11aca4bfb295b55e09e9e
Jan Dockx <Jan_Dockx@peopleware.be>
2013-04-23 11:56:32 +0000

    fixed copy error
    git-svn-id: https://ppwcode.googlecode.com/svn/javascript/util/oddsAndEnds/trunk@6764 463e38ab-4f4e-0410-907a-65d2b3888964

--- src/oddsAndEnds/ui/Output.js
+++ src/oddsAndEnds/ui/Output.js
@@ -1,6 +1,6 @@
 define(["../../../dojo/_base/declare",
         "dojox/mvc/Output",
-        "./xml"],
+        "../xml"],
   function(declare, DojoxMvcOutput, xml) {
 
     return declare([DojoxMvcOutput], {
