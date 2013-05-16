81ee351bfd8c4ea137f52ca5e25c6b9b115ff531
Jan Dockx <Jan_Dockx@peopleware.be>
2013-05-16 22:31:38 +0000

    this is about it for the demonstration of a widget that can change locale the only remaining sad part is that we need to bind all the languages, and the strange converter
    git-svn-id: https://ppwcode.googlecode.com/svn/javascript/util/oddsAndEnds/trunk@6808 463e38ab-4f4e-0410-907a-65d2b3888964

--- src/oddsAndEnds/test/changeLang/Widget.js
+++ src/oddsAndEnds/test/changeLang/Widget.js
@@ -1,13 +1,66 @@
 define(["dojo/_base/declare",
         "dijit/_WidgetBase", "dijit/_TemplatedMixin", "dijit/_WidgetsInTemplateMixin",
-        "dojo/Stateful", "dojo/number", "dojo/_base/lang",
-  "dojo/text!./widget.html", "dojo/i18n", "module",
+        "dojo/Stateful", "dojo/number", "dojo/_base/lang", "dojox/mvc/resolve", "dojo/aspect",
+        "dojo/text!./widget.html", "dojo/i18n", "require", "module",
         "dojo/i18n!./nls/labels",
         "dojox/mvc/at", "dojox/mvc/Group", "../../ui/Output", "dijit/form/TextBox"],
   function(declare,
            WidgetBase, TemplatedMixin, WidgetsInTemplateMixin,
-           Stateful, number, lang,
-           template, i18n, module) {
+           Stateful, number, lang, resolve, aspect,
+           template, i18n, require, module) {
+
+    function getParent(/*dijit/_WidgetBase*/ w){
+      // summary:
+      //		Returns parent widget having data binding target for relative data binding.
+      // w: dijit/_WidgetBase
+      //		The widget.
+
+      // Usage of dijit/registry module is optional. Return null if it's not already loaded.
+      var registry;
+      try{
+        registry = require("dijit/registry");
+      }catch(e){
+        return;
+      }
+      var pn = w.domNode && w.domNode.parentNode, pw, pb;
+      while(pn){
+        pw = registry.getEnclosingWidget(pn);
+        if(pw){
+          var relTargetProp = pw._relTargetProp || "target", pt = lang.isFunction(pw.get) ? pw.get(relTargetProp) : pw[relTargetProp];
+          if(pt || relTargetProp in pw.constructor.prototype){
+            return pw; // dijit/_WidgetBase
+          }
+        }
+        pn = pw && pw.domNode.parentNode;
+      }
+    }
+
+    // Monkey patch dijit._WidgetBase._setLangAttr to refresh bound values
+    aspect.after(WidgetBase.prototype, "_setLangAttr", function(result) {
+      if (this._refs) {
+        for (var prop in this._refs) {
+          if (prop != "lang") {
+            var binding = this._refs[prop];
+            var converter = binding.converter;
+            if (converter && converter.format) {
+              var parent = getParent(this);
+              var relModelProp = parent && parent._relTargetProp || "target";
+              var relModel = parent && parent.get(relModelProp);
+              var resolvedModel = resolve(binding.target, relModel);
+              if (resolvedModel) {
+                var converterInstance = {source: resolvedModel, target: this};
+                var formatFunc = lang.hitch(converterInstance, converter.format);
+                var constraints = lang.mixin({}, this.constraints, resolvedModel.constraints);
+                var currentValue = resolvedModel.get(binding.targetProp);
+                var convertedValue = formatFunc.call(converterInstance, currentValue, constraints);
+                this.set(prop, convertedValue);
+              }
+            }
+          }
+        }
+      }
+      return result;
+    });
 
     return declare([WidgetBase, TemplatedMixin, WidgetsInTemplateMixin], {
 
@@ -26,33 +79,12 @@ define(["dojo/_base/declare",
       },
 
       _setLangAttr: function(value) {
-        this._set("lang", value);
+        this.inherited(arguments);
         var labels = i18n.getLocalization("ppwcode/oddsAndEnds/test/changeLang", "labels", value);
         this.set("labels", labels);
-        if (this.get("target")) {
-          var old = this.get("target").get("aNumber");
-          this.get("target").set("aNumber", null);
-          this.get("target").set("aNumber", old);
-          // note:
-          //   Triggering a re-render of a binding is next to impossible.
-          //   In dojox/mvc/sync, there is a test for the old value to be different from the new value,
-          //   before it makes an attempt to re-render.
-          //   Since on language change, the value does not differ, but only its representation,
-          //   there is no re-rendering.
-          //   This should be considered a bug. A workaround is to change the value to another value
-          //   twice.
-          //
-          //   But then again, we cannot do this on immutable objects.
-          //
-          //   Second, we need to do this for i18n-dependent representations.
-          //   How do we find those? List them here explicitly?
-          //
-          //   It will be better to set the value of the widgets ourselfs, converted.
-          //   But in that case, how do we get at the converter?
-        }
       },
 
-      aNumberConverter: function() {
+      aNumberConverter: {
         // note:
         //   This converter is here solely because the options that we define in the template
         //   or created and locked on widget instance construction.
@@ -70,30 +102,42 @@ define(["dojo/_base/declare",
         //   could then use this.lang (this being the TextBox).
         //   The other way is to encapsulate the converter in a function, like we do here,
         //   to defined a "self = this", to acces the lang later.
-        var self = this;
-        return {
-          format: function(double, options) {
-            if (!double && (double !== 0)) {
-              return options && options.na ? options.na : 'N/A';
-            }
-            else {
-              var opt = lang.clone(options);
-              opt.locale = self.lang;
-              return number.format(double, opt);
-            }
-          },
-          parse: function(str, options) {
-            if (!str || str === (options && options.na ? options.na : 'N/A')) {
-              return null;
-            }
-            else {
-              // with options.places, we need to supply str with this exact decimal places!!! 0's aren't added, number isn't truncated
-              var opt = lang.clone(options);
-              delete opt.places;
-              opt.locale = self.lang;
-              var d = number.parse(str, opt);
-              return d;
-            }
+
+        // note:
+        //   Now we have cycles:
+        //   source.aNumber = PI
+        //   change lang
+        //   we reset to PI
+        //   format cuts to 3 places
+        //   this change in text box triggers event
+        //   parse to number 3.145
+        //   this is set on source
+        //   this change triggers format
+        //   "3.145" is set again
+
+        //   the equals method should compare formatted version, not numbers
+
+        format: function(d, options) {
+          if (!d && (d !== 0)) {
+            return options && options.na ? options.na : 'N/A';
+          }
+          else {
+            var opt = lang.clone(options);
+            opt.locale = this.target.lang;
+            return number.format(d, opt);
+          }
+        },
+        parse: function(str, options) {
+          if (!str || str === (options && options.na ? options.na : 'N/A')) {
+            return null;
+          }
+          else {
+            // with options.places, we need to supply str with this exact decimal places!!! 0's aren't added, number isn't truncated
+            var opt = lang.clone(options);
+            delete opt.places;
+            opt.locale = this.target.lang;
+            var d = number.parse(str, opt);
+            return d;
           }
         }
       }
