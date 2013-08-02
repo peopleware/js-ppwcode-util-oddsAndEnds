1b1c1b86c9400db828f1a7268361d9714a316a7b
Jan Dockx <Jan_Dockx@peopleware.be>
2013-08-02 08:52:48 +0000

    Now works (from first use)
    git-svn-id: https://ppwcode.googlecode.com/svn/javascript/util/oddsAndEnds/trunk@6913 463e38ab-4f4e-0410-907a-65d2b3888964

--- src/oddsAndEnds/ui/Optional.js
+++ src/oddsAndEnds/ui/Optional.js
@@ -1,5 +1,5 @@
-define(["../../../dojo/_base/declare", "dijit/_WidgetBase", "dojo/_base/dom-style", "dojo/fx"],
-  function(declare, _WidgetBase, domStyle, fx) {
+define(["dojo/_base/declare", "dijit/_WidgetBase", "dojo/dom-style", "dojo/fx/Toggler", "dojo/fx"],
+  function(declare, _WidgetBase, domStyle, FxToggler, fx) {
 
     return declare([_WidgetBase], {
       // summary:
@@ -14,9 +14,24 @@ define(["../../../dojo/_base/declare", "dijit/_WidgetBase", "dojo/_base/dom-styl
       // displayed: boolean
       displayed: false,
 
+      // reversed: boolean
+      //   With a binding on `displayed`, certainly if it is several levels deep, the first
+      //   actual `set("displayed")` happens quite late. `displayed` is initially false,
+      //   so nothing is shown until the first `set("displayed")` happens in that case.
+      //   With `reverse` true, we reverse the behavior: we show if `displayed` is false, and
+      //   and hide if `displayed` is true. This also often makes the binding easier
+      //   (no need for a format function).
+      reversed: false,
+
       postCreate: function() {
         this.inherited(arguments);
-        domStyle.set(this.domNode, "display", "none");
+        if (!this.shown()) {
+          domStyle.set(this.domNode, "display", "none");
+        }
+      },
+
+      shown: function() {
+        return this.reversed ? !this.displayed : this.displayed;
       },
 
       _setDisplayedAttr: function(displayed) {
@@ -25,16 +40,22 @@ define(["../../../dojo/_base/declare", "dijit/_WidgetBase", "dojo/_base/dom-styl
         //   effect takes time, this returns a Promise. Use it or not, as you like.
         //   Returns null if the value is not changed.
 
-        if (this.displayed == displayed) {
-          this._set("displayed", displayed);
-          displayed ? this._animation().show() : this._animation().hide();
+        var booleanDisplayed = !!displayed; // turn truthy or falsy into true or false
+        if (booleanDisplayed !== this.displayed) {
+          this._set("displayed", booleanDisplayed);
+          if (this.shown()) {
+            this._animation().show()
+          }
+          else {
+            this._animation().hide();
+          }
         }
       },
 
       _animation: function() {
         //noinspection JSUnresolvedFunction
-        return new fx.Toggler({
-          node: self.domNode,
+        return new FxToggler({
+          node: this.domNode,
           showDuration: 500,
           hideDuration: 500,
           showFunc: fx.wipeIn,
