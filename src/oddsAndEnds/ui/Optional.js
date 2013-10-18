bb6cb02a6858d49e8b73521e0aa0c138b52e4b1b
Jan Dockx <Jan_Dockx@peopleware.be>
2013-10-18 17:16:57 +0000

    Optional without animations.
    git-svn-id: https://ppwcode.googlecode.com/svn/javascript/util/oddsAndEnds/trunk@7210 463e38ab-4f4e-0410-907a-65d2b3888964

--- src/oddsAndEnds/ui/Optional.js
+++ src/oddsAndEnds/ui/Optional.js
@@ -1,5 +1,5 @@
-define(["dojo/_base/declare", "dijit/_WidgetBase", "dojo/dom-style", "dojo/fx/Toggler", "dojo/fx"],
-  function(declare, _WidgetBase, domStyle, FxToggler, fx) {
+define(["dojo/_base/declare", "dijit/_WidgetBase", "dojo/dom-style"],
+  function(declare, _WidgetBase, domStyle) {
 
     return declare([_WidgetBase], {
       // summary:
@@ -7,9 +7,7 @@ define(["dojo/_base/declare", "dijit/_WidgetBase", "dojo/dom-style", "dojo/fx/To
       //   If `displayed` is set to true, the widget appears. If `displayed` is set
       //   to false, it disappears. The default is not-displayed.
       // description:
-      //   Changing the state is done with an animation. Although `set("displayed", value)`
-      //   returns immediately, the operation takes a while.
-      //   For now, there is no option to get a Promise on the animation.
+      //   Changing the state is not done with an animation.
 
       // displayed: boolean
       displayed: false,
@@ -23,27 +21,9 @@ define(["dojo/_base/declare", "dijit/_WidgetBase", "dojo/dom-style", "dojo/fx/To
       //   (no need for a format function).
       reversed: false,
 
-      // _showAnimation: Animation
-      _showAnimation: null,
-
-      // _hideAnimation: Animation
-      _hideAnimation: null,
-
-      _animateDelayHandler: null,
-      _lastRenderedShown: false,
-
       postCreate: function() {
         this.inherited(arguments);
-        if (this.shown()) {
-          domStyle.set(this.domNode, "display", "");
-          this._lastRenderedShown = true;
-        }
-        else {
-          domStyle.set(this.domNode, "display", "none");
-          this._lastRenderedShown = false;
-        }
-        this._showAnimation = fx.wipeIn({node: this.domNode, duration: 500});
-        this._hideAnimation = fx.wipeOut({node: this.domNode, duration: 500});
+        this._setDisplay();
       },
 
       shown: function() {
@@ -57,49 +37,17 @@ define(["dojo/_base/declare", "dijit/_WidgetBase", "dojo/dom-style", "dojo/fx/To
         //   Returns null if the value is not changed.
 
         var booleanDisplayed = !!displayed; // turn truthy or falsy into true or false
-        if (booleanDisplayed != !!this.displayed) { // somebody DOES insert null into this.displayed directly; make sure we are comparing booleans
-          this._set("displayed", booleanDisplayed);
-          this._animate();
-//          if (this.shown()) {
-//            this._hideAnimation.stop();
-//            this._showAnimation.play();
-////            domStyle.set(this.domNode, "display", "");
-//          }
-//          else {
-//            this._showAnimation.stop();
-//            this._hideAnimation.play();
-////            domStyle.set(this.domNode, "display", "none");
-//          }
-        }
+        this._set("displayed", booleanDisplayed);
+        this._setDisplay();
       },
 
-      _animate: function() {
-        // summary:
-        //   We start the animation with a delay, so that in that case of many fast switches,
-        //   we get no flicker.
-
-        if (this._animateDelayHandler) { // we were called in quick succession
-          this._animateDelayHandler.remove();
-          this._animateDelayHandler = null;
+      _setDisplay: function() {
+        if (this.shown()) {
+          domStyle.set(this.domNode, "display", "");
         }
-
-        this._animateDelayHandler = this.defer(
-          function() {
-            this._animateDelayHandler = null;
-            if (this.shown() && !this._lastRenderedShown) {
-              this._hideAnimation.stop();
-              this._lastRenderedShown = true;
-              this._showAnimation.play();
-            }
-            else if (!this.shown() && this._lastRenderedShown) {
-              this._showAnimation.stop();
-              this._lastRenderedShown = false;
-              this._hideAnimation.play();
-            }
-            // else NOP; we are where we need to be already
-          },
-          200
-        );
+        else {
+          domStyle.set(this.domNode, "display", "none");
+        }
       }
 
     });
