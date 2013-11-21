f051aa213f31e9e49cd6eeafa52ed3f35ca62e63
Jan Dockx <Jan_Dockx@peopleware.be>
2013-11-21 11:19:56 +0000

    preparing for I 1.0 release Fixed some errors and warnings (that are not errors are warnings).
    git-svn-id: https://ppwcode.googlecode.com/svn/javascript/util/oddsAndEnds/trunk@7281 463e38ab-4f4e-0410-907a-65d2b3888964

--- src/oddsAndEnds/test/changeLang/Widget.js
+++ src/oddsAndEnds/test/changeLang/Widget.js
@@ -1,3 +1,19 @@
+/*
+Copyright 2012 - $Date $ by PeopleWare n.v.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 define(["dojo/_base/declare",
         "dijit/_WidgetBase", "dijit/_TemplatedMixin", "dijit/_WidgetsInTemplateMixin",
         "dojo/number", "dojo/_base/lang", "dojox/mvc/resolve", "dojo/aspect",
@@ -41,6 +57,7 @@ define(["dojo/_base/declare",
       if (this._refs) {
         for (var prop in this._refs) {
           if (prop != "lang") {
+            //noinspection JSUnfilteredForInLoop
             var binding = this._refs[prop];
             var converter = binding.converter;
             if (converter && converter.format) {
@@ -54,6 +71,7 @@ define(["dojo/_base/declare",
                 var constraints = lang.mixin({}, this.constraints, resolvedModel.constraints);
                 var currentValue = resolvedModel.get(binding.targetProp);
                 var convertedValue = formatFunc.call(converterInstance, currentValue, constraints);
+                //noinspection JSUnfilteredForInLoop
                 this.set(prop, convertedValue);
               }
             }
