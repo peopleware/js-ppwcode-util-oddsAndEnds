8d8130f771e1e6026425938c024a4e24ce9f70b0
Jan Dockx <Jan_Dockx@peopleware.be>
2014-02-14 10:34:37 +0000

    now this seems to be ok for FF
    git-svn-id: https://ppwcode.googlecode.com/svn/javascript/util/oddsAndEnds/trunk@7446 463e38ab-4f4e-0410-907a-65d2b3888964

--- src/oddsAndEnds/page/_Proxy.js
+++ src/oddsAndEnds/page/_Proxy.js
@@ -15,13 +15,17 @@
  */
 
 define(["dojo/_base/declare", "dojo/sniff", "dojo/Deferred", "./_sharedKeys",
+        "ppwcode-util-oddsAndEnds/promise/Arbiter",
         "ppwcode-util-oddsAndEnds/log/logger!", "module"],
   function (declare, has, Deferred, _sharedKeys,
+            Arbiter,
             logger, module) {
 
     var LOAD_TIMEOUT = 30000;
     var POLL_INTERVAL = 200;
     var MAX_POLLS = 150; // 30 seconds at least
+
+    var loadArbiter = new Arbiter();
 
     function randomName() {
       // summary:
@@ -147,174 +151,180 @@ define(["dojo/_base/declare", "dojo/sniff", "dojo/Deferred", "./_sharedKeys",
         //   If something goes wrong, all bets are off.
 
         var self = this;
-        var deferred = new Deferred();
+        return loadArbiter.guard(
+          self._name,
+          function() {
+            var deferred = new Deferred();
 
-        function prepareForLoad() {
-          /* ProxiedWindow now is blank and has the correct name;
-           Load it, and wait for it to finish initialization; then
-           report success or failure.
-           The Page in the window will call a success or failure callback in this window.
-           This is the only way that works in all browsers (postMessage cannot be used, because
-           listeners cannot reliably be registered before the page is loaded. This creates
-           a race condition. */
+            function prepareForLoad() {
+              /* ProxiedWindow now is blank and has the correct name;
+               Load it, and wait for it to finish initialization; then
+               report success or failure.
+               The Page in the window will call a success or failure callback in this window.
+               This is the only way that works in all browsers (postMessage cannot be used, because
+               listeners cannot reliably be registered before the page is loaded. This creates
+               a race condition. */
 
-          logger.debug("focus: Preparing for load.");
-          var timeout;
+              logger.debug("focus: Preparing for load.");
+              var timeout;
 
-          function removeCallBacks() {
-            if (timeout) {
-              clearTimeout(timeout);
+              function removeCallBacks() {
+                if (timeout) {
+                  clearTimeout(timeout);
+                }
+                delete window[_sharedKeys.SUCCESS_CALLBACK_NAME + "_" + self._name];
+                delete window[_sharedKeys.ERROR_CALLBACK_NAME + "_" + self._name];
+              }
+
+              function failure(msg, rejection) {
+                removeCallBacks();
+                logger.error("Window[" + self._name + "] " + msg);
+                deferred.reject(rejection);
+              }
+
+              window[_sharedKeys.SUCCESS_CALLBACK_NAME + "_" + self._name] = function(/*Page*/ page) {
+                removeCallBacks();
+                logger.info("Window[" + self._name + "] loaded");
+                deferred.resolve(page);
+              };
+
+              window[_sharedKeys.ERROR_CALLBACK_NAME + "_" + self._name] = function(errMsg) {
+                failure("failed to open", errMsg);
+              };
+
+              timeout = setTimeout(
+                function() {
+                  failure("loading timed-out.", "LOADING WINDOW TIMED OUT");
+                },
+                LOAD_TIMEOUT
+              );
             }
-            delete window[_sharedKeys.SUCCESS_CALLBACK_NAME + "_" + self._name];
-            delete window[_sharedKeys.ERROR_CALLBACK_NAME + "_" + self._name];
-          }
 
-          function failure(msg, rejection) {
-            removeCallBacks();
-            logger.error("Window[" + self._name + "] " + msg);
-            deferred.reject(rejection);
-          }
+            if (!self._proxiedWindow) {
+              logger.debug("focus: no proxiedWindow yet.");
+            }
+            else {
+              logger.debug("focus: there is a proxiedWindow reference.");
+              /* We opened the window earlier. But,
+                 - it might be closed already;
+                 - it might have a different name now;
+                 - it might now contain a different URL;
+                 - or it might be open still, have the correct name, and contain the correct URL
 
-          window[_sharedKeys.SUCCESS_CALLBACK_NAME + "_" + self._name] = function(/*Page*/ page) {
-            removeCallBacks();
-            logger.info("Window[" + self._name + "] loaded");
-            deferred.resolve(page);
-          };
+                 If it is still open, and has the correct name and URL, we just want to return the
+                 existing reference. We cannot guarantee to bring it to the front in IE and FF.
 
-          window[_sharedKeys.ERROR_CALLBACK_NAME + "_" + self._name] = function(errMsg) {
-            failure("failed to open", errMsg);
-          };
+                 If it is closed already, we want to discard our old reference, open a new window,
+                 and load the correct URL in that window. It will be brought to the front.
 
-          timeout = setTimeout(
-            function() {
-              failure("loading timed-out.", "LOADING WINDOW TIMED OUT");
-            },
-            LOAD_TIMEOUT
-          );
-        }
+                 When a SecurityError occurs when we access the name property, we know also
+                 that the window has a different URL.
+                 If it is there still, but contains a different URL, we want to reload the correct
+                 URL. It would not be brought to the front.
+                 If it contains the correct URL but has a different name, we might re-appropriate it,
+                 but we will not. This should not happen, and this would be confusing.
+                 We might be more consistent in this case by closing the window, and reopening it
+                 (it then becomes the second case, where it is brought to the front). However,
+                 closing the window can only be done with windows that where opened by a script.
+                 An "original" window cannot be changed like that. In that case, we might
+                 change the name of the original window, and still open a new one. The window
+                 is no longer "ours" anyway. And it can stay open. */
+              if (!self._proxiedWindow.closed && self._proxiedWindowHasCorrectUrlAndName()) {
+                logger.debug("focus: proxiedWindow reference has correct name and url and is not closed. We are done.");
+                self._proxiedWindow.focus(); // will not always work in FF and IE
+                self._waitForPage(deferred, 0);
+                return deferred.promise;
+              }
+              // closed or not the correct URL or wrong name
+              logger.debug("focus: proxiedWindow has wrong name or url, or is closed. Discarding it.");
+              if (!self._proxiedWindow.closed) {
+                // not the correct URL or wrong name
+                // rename the window and discard the reference
+                try {
+                  self._proxiedWindow.name = randomName();
+                }
+                catch (err) {
+                  if (isSecurityError(err)) {
+                    logger.debug("proxiedWindow has a new URL from another origin. Will re-appropriate.");
+                    /* If the URL is another origin, we will get a SecurityError.
+                     We cannot change the name, and cannot gain control of that window.
+                     The only thing we can do is regain control by "opening" the window
+                     with the same name and a given URL directly. */
+                    prepareForLoad();
+                    self._proxiedWindow = window.open(self._href, self._name);
+                    return deferred.promise;
+                  }
+                  throw err;
+                }
+              }
+              // in any case, discard and load again now
+              self._proxiedWindow = null;
+            }
 
-        if (!self._proxiedWindow) {
-          logger.debug("focus: no proxiedWindow yet.");
-        }
-        else {
-          logger.debug("focus: there is a proxiedWindow reference.");
-          /* We opened the window earlier. But,
-             - it might be closed already;
-             - it might have a different name now;
-             - it might now contain a different URL;
-             - or it might be open still, have the correct name, and contain the correct URL
-
-             If it is still open, and has the correct name and URL, we just want to return the
-             existing reference. We cannot guarantee to bring it to the front in IE and FF.
-
-             If it is closed already, we want to discard our old reference, open a new window,
-             and load the correct URL in that window. It will be brought to the front.
-
-             When a SecurityError occurs when we access the name property, we know also
-             that the window has a different URL.
-             If it is there still, but contains a different URL, we want to reload the correct
-             URL. It would not be brought to the front.
-             If it contains the correct URL but has a different name, we might re-appropriate it,
-             but we will not. This should not happen, and this would be confusing.
-             We might be more consistent in this case by closing the window, and reopening it
-             (it then becomes the second case, where it is brought to the front). However,
-             closing the window can only be done with windows that where opened by a script.
-             An "original" window cannot be changed like that. In that case, we might
-             change the name of the original window, and still open a new one. The window
-             is no longer "ours" anyway. And it can stay open. */
-          if (!self._proxiedWindow.closed && self._proxiedWindowHasCorrectUrlAndName()) {
-            logger.debug("focus: proxiedWindow reference has correct name and url and is not closed. We are done.");
-            self._proxiedWindow.focus(); // will not always work in FF and IE
-            self._waitForPage(deferred, 0);
-            return deferred.promise;
-          }
-          // closed or not the correct URL or wrong name
-          logger.debug("focus: proxiedWindow has wrong name or url, or is closed. Discarding it.");
-          if (!self._proxiedWindow.closed) {
-            // not the correct URL or wrong name
-            // rename the window and discard the reference
+            // The window we want might already be open. Lets see if we can find it by name.
+            logger.debug("focus: Finding or opening new proxied window.");
             try {
-              self._proxiedWindow.name = randomName();
+              self._proxiedWindow = window.open("", self._name);
+              /* Returns a reference to the existing window with name _name,
+                 or opens a new one with that name if none exists yet.
+                 The location of the window is empty for a new one, and does not change
+                 for an existing one. Returns falsy if opening fails.
+                 The window is not closed, but might still have the correct URL,
+                 an different URL, or be blank.
+                 At least, that this the theory.
+                 - On Firefox, this seems to be correct.
+                 - On Webkit, we only find an existing window if it was first created
+                   FROM THIS WINDOW. Otherwise, it is as if the other window does not exist.
+                   There seems to be NO WAY to retrieve a reference to a window that we
+                   haven't first opened.
+                 - On IE, it is as described EXCEPT when the URL changed to another origin.
+                   Then this function returns an exception "Access is denied.".
+                   We can never get a reference to that window.
+                 In both of the exception cases, we will proceed as if the window was not found.
+                 */
             }
-            catch (err) {
-              if (isSecurityError(err)) {
+            catch (err) { // only for IE
+              if (has("trident") && isSecurityError(err)) {
+                log.debug("Cannot use existing window with name " + self._name + " because of origin. " +
+                          "Creating a brand new window.");
+                prepareForLoad();
+                self._proxiedWindow = window.open(self._href, self._name);
+                return deferred.promise;
+              }
+              else {
                 throw err;
               }
-              logger.debug("proxiedWindow has a new URL from another origin. Will re-appropriate.");
-              /* If the URL is another origin, we will get a SecurityError.
-               We cannot change the name, and cannot gain control of that window.
-               The only thing we can do is regain control by "opening" the window
-               with the same name and a given URL directly. */
+            }
+            if (!self._proxiedWindow) {
+              logger.error("Window[" + self._name + "] failed to open");
+              deferred.reject("WINDOW COULD NOT BE OPENED");
+            }
+            else if (self._proxiedWindowHasCorrectUrlAndName()) { // name is guaranteed to be correct (barring race conditions)
+              logger.debug("focus: Found window with correct name and URL. We are done.");
+              self._proxiedWindow.focus(); // will not always work in FF and IE
+              self._waitForPage(deferred, 0);
+            }
+            else {
+              // not the correct URL or blank; we need to reload in any case, re-appropriating it if it is not blank
+              logger.debug("focus: Found new window or window with wrong URL. (Re-)appropriating and loading.");
               prepareForLoad();
-              self._proxiedWindow = window.open(this._href, this._name);
-              return deferred.promise;
+              // window.open(this._href, this._name); will create a new Window in Chrome, and not reuse the existing window.
+              // This is only a last resort.
+              try {
+                self._proxiedWindow.location.replace(self._href);
+              }
+              catch (err) {
+                if (isSecurityError(err)) {
+                  log.debug("Cannot change the location of the found window with name " + self._name + " because of origin. " +
+                            "Creating a brand new window or re-appropriating it.");
+                  self._proxiedWindow = window.open(self._href, self._name);
+                }
+              }
             }
-          }
-          // in any case, discard and load again now
-          self._proxiedWindow = null;
-        }
-
-        // The window we want might already be open. Lets see if we can find it by name.
-        logger.debug("focus: Finding or opening new proxied window.");
-        try {
-          self._proxiedWindow = window.open("", this._name);
-          /* Returns a reference to the existing window with name _name,
-             or opens a new one with that name if none exists yet.
-             The location of the window is empty for a new one, and does not change
-             for an existing one. Returns falsy if opening fails.
-             The window is not closed, but might still have the correct URL,
-             an different URL, or be blank.
-             At least, that this the theory.
-             - On Firefox, this seems to be correct.
-             - On Webkit, we only find an existing window if it was first created
-               FROM THIS WINDOW. Otherwise, it is as if the other window does not exist.
-               There seems to be NO WAY to retrieve a reference to a window that we
-               haven't first opened.
-             - On IE, it is as described EXCEPT when the URL changed to another origin.
-               Then this function returns an exception "Access is denied.".
-               We can never get a reference to that window.
-             In both of the exception cases, we will proceed as if the window was not found.
-             */
-        }
-        catch (err) { // only for IE
-          if (has("trident") && isSecurityError(err)) {
-            log.debug("Cannot use existing window with name " + this._name + " because of origin. " +
-                      "Creating a brand new window.");
-            prepareForLoad();
-            self._proxiedWindow = window.open(this._href, this._name);
             return deferred.promise;
-          }
-          else {
-            throw err;
-          }
-        }
-        if (!self._proxiedWindow) {
-          logger.error("Window[" + self._name + "] failed to open");
-          deferred.reject("WINDOW COULD NOT BE OPENED");
-        }
-        else if (self._proxiedWindowHasCorrectUrlAndName()) { // name is guaranteed to be correct (barring race conditions)
-          logger.debug("focus: Found window with correct name and URL. We are done.");
-          self._proxiedWindow.focus(); // will not always work in FF and IE
-          self._waitForPage(deferred, 0);
-        }
-        else {
-          // not the correct URL or blank; we need to reload in any case, re-appropriating it if it is not blank
-          logger.debug("focus: Found new window or window with wrong URL. (Re-)appropriating and loading.");
-          prepareForLoad();
-          // window.open(this._href, this._name); will create a new Window in Chrome, and not reuse the existing window.
-          // This is only a last resort.
-          try {
-            self._proxiedWindow.location.replace(this._href);
-          }
-          catch (err) {
-            if (isSecurityError(err)) {
-              log.debug("Cannot change the location of the found window with name " + this._name + " because of origin. " +
-                        "Creating a brand new window or re-appropriating it.");
-              self._proxiedWindow = window.open(this._href, this._name);
-            }
-          }
-        }
-        return deferred.promise;
+          },
+          true
+        );
       },
 
       call: function(name, args) {
